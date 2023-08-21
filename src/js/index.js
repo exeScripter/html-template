76646760df56dc03ab4622332d9776b0e4873b6d
naroors <68824919+exeScripter@users.noreply.github.com>
2023-08-21 18:58:15 +0200

    Script

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+/*
+    Made for https://git.naroors.pl/
+    Public Usage Allowed, 
+    MIT Licensed.
+
+
+    Something doesn't work/Want to suggest some changes? 
+    Feel free to open an issue on GitHub:
+    https://git.naroors.pl/issues
+*/
+
+console.log("You are using the latest version of the file!")
